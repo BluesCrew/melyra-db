6b0de207e3a88e57dba7baa4f31d0808339c7e33
TheCarotte <kaszycaadelin@gmail.com>
2024-03-02 00:15:45 +0100

    tooltip lore test

--- editors/items/populate_list.js
+++ editors/items/populate_list.js
@@ -6,7 +6,7 @@ a = new BaseItem(
     {
         isCustomTexture: false,
         rarityColor: RARITY_UNCOMMON,
-        minecraftId: "cooked_beef"
+        minecraftId: "wheat"
     }
 );
 
@@ -35,7 +35,17 @@ c = new BaseItem(
 );
 
 all_items = [];
-all_items.push(a, b, c, c, a, b, a, c, a, b, a, c, a, c, b, c, b);
+all_items.push(a);
+
+
+const template_lore = [
+    {color:"#FFFFFF",text:"[Melee | Uncommon]"},
+    {text:" "},
+    {color:"#555555", text:"Test Description, first line"},
+    {color:"#555555", text:"Test Description, second line"},
+    {text:" "},
+    [{color:"#FF5555",text:"❤"},{color:"#AAAAAA",text:" Health"},{color:"#FFFFFF",text:" 1"}]
+]
 
 function populate_list(item) {
     let parent = document.getElementById("list-parent");
@@ -55,8 +65,72 @@ function populate_list(item) {
     newElem.appendChild(img);
 
     parent.appendChild(newElem);
+
+    parent.addEventListener("mouseleave", function() {
+        setTimeout(function() {
+            newName.innerHTML = '<span style="color: rgb(85, 255, 255); text-align: center;">Test Name</span>';
+        }, 400)
+    })
+
+    parent.addEventListener("mouseenter", function () {
+        newName.innerHTML = 
+        '<span style="color: rgb(85, 255, 255); text-align: center;">Test Name</span>\
+        <br>\
+        <span style="color: rgb(237, 237, 237);">[Helmet | Legendary]</span>\
+        <br>\
+        <br>\
+        <span class="italic" style="color: rgb(85, 85, 85);">Test description </span>\
+        <br>\
+        <span class="italic" style="color: rgb(85, 85, 85);"> Second line of test description</span>\
+        <br>\
+        <br>\
+        <span style="color: rgb(255, 85, 85);">❤ </span>\
+        <span style="color: rgb(170, 170, 170);">Health </span>\
+        <span style="color: rgb(255, 255, 255);">1</span>\
+        <br>\
+        <span style="color: rgb(85, 255, 85);">❂ </span>\
+        <span style="color: rgb(170, 170, 170);">Defense </span>\
+        <span style="color: rgb(255, 255, 255);">2</span>\
+        <span style="color: rgb(255, 255, 255);"> - </span>\
+        <span style="color: rgb(255, 255, 255);">3</span>\
+        <br>\
+        <br>\
+        <span style="color: rgb(85, 255, 255);">Display Ability Name:</span>\
+        <span class="bold" style="color: rgb(255, 255, 85);"> [</span>\
+        <span style="color: rgb(255, 170, 0);">Full Set</span>\
+        <span class="bold" style="color: rgb(255, 255, 85);">]</span>\
+        <br>\
+        <span style="color: rgb(170, 170, 170);">Ability Description</span>\
+        <br>\
+        <span style="color: rgb(85, 255, 255);"> ₪ Mana: </span>\
+        <span style="color: rgb(255, 255, 255);">2</span>\
+        <br>\
+        <br>\
+        <span style="color: rgb(85, 255, 255);">||</span>\
+        <span style="color: rgb(85, 255, 255);"> Enchantments</span>\
+        <br>\
+        <span style="color: rgb(85, 255, 255);">||</span>\
+        <span style="color: rgb(170, 170, 170);"> [</span>\
+        <span style="color: rgb(255, 255, 255);">❌</span>\
+        <span style="color: rgb(170, 170, 170);">] </span>\
+        <span style="color: rgb(170, 170, 170);"> [</span>\
+        <span style="color: rgb(255, 255, 255);">❌</span>\
+        <span style="color: rgb(170, 170, 170);">] </span>\
+        <span style="color: rgb(170, 170, 170);"> [</span>\
+        <span style="color: rgb(255, 255, 255);">❌</span>\
+        <span style="color: rgb(170, 170, 170);">] </span>\
+        <span style="color: rgb(170, 170, 170);"> [</span>\
+        <span style="color: rgb(255, 255, 255);">❌</span>\
+        <span style="color: rgb(170, 170, 170);">] </span>\
+        <span style="color: rgb(170, 170, 170);"> [</span>\
+        <span style="color: rgb(255, 255, 255);">❌</span>\
+        <span style="color: rgb(170, 170, 170);">] </span>\
+        <br>'
+    })
 }
 
-all_items.forEach(element => {
-    populate_list(element);
-});
+
+for(item of all_items) 
+{
+    populate_list(item);
+}
