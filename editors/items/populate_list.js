3e48ca94352db5dfae2b3a60d4ea271604708ac0
TheCarotte <76431789+TheCarotte@users.noreply.github.com>
2024-03-01 01:24:19 +0100

    added arbitrary item list population

--- editors/items/populate_list.js
+++ editors/items/populate_list.js
@@ -1,4 +1,42 @@
 diamond_sword = {
     name: "Diamond Sword",
-    rarity_color: ""
+    id: "diamond_sword",
+    rarity_color: "#FFFFFF"
 }
+sword_of_stars = {
+    name: "Sword of Stars",
+    id: "golden_sword",
+    rarity_color: "#55FFFF"
+}
+
+
+all_items = [];
+all_items.push(diamond_sword);
+all_items.push(sword_of_stars);
+
+function get_sprite(item) {
+    return "/melyra-db/assets/prototype/" + item.id + ".png";
+}
+
+function populate_list(item) {
+    let parent = document.getElementById("list-parent");
+    console.log(parent)
+
+    let newElem = document.createElement("a");
+    newElem.className = "melyra-element";
+    let newName = document.createElement("p");
+    newName.textContent = item.name;
+    newName.style.color = item.rarity_color;
+    
+    let img = document.createElement("img");
+    img.src = get_sprite(item);
+    
+    newElem.appendChild(newName);
+    newElem.appendChild(img);
+
+    parent.appendChild(newElem);
+}
+
+all_items.forEach(element => {
+    populate_list(element);
+});
